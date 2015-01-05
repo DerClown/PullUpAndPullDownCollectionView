44a7f7507504cb938445d32788715e8f7e972b00
DongZai <839235027@qq.com>
2015-01-05 18:12:19 +0800

    AutoLayout

--- BaseCollectionView/CollectionView/CollectionRefreshFooterView.m
+++ BaseCollectionView/CollectionView/CollectionRefreshFooterView.m
@@ -24,20 +24,25 @@
 - (void)config {
     //这里我用你最习惯的方式写
     if (!_indicatorView) {
-        _indicatorView = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+        _indicatorView = [[UIActivityIndicatorView alloc] init];
         _indicatorView.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
         [self addSubview:_indicatorView];
     }
     
     if (!_statusLabel) {
-        _statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, (self.frame.size.height - 22) / 2, self.frame.size.width, 22)];
+        _statusLabel = [[UILabel alloc] init];
         _statusLabel.backgroundColor = [UIColor clearColor];
         _statusLabel.text = @"加载更多";
         _statusLabel.textColor = [UIColor grayColor];
-        _statusLabel.textAlignment = NSTextAlignmentCenter;
         _statusLabel.font = [UIFont boldSystemFontOfSize:20];
         [self addSubview:_statusLabel];
     }
+    
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_statusLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:10]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_statusLabel attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
+    
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_indicatorView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:_statusLabel attribute:NSLayoutAttributeLeft multiplier:1.0 constant:-10]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_indicatorView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
 }
 
 #pragma mark - Setter
@@ -50,8 +55,6 @@
             break;
         case PullUpRefreshLoading:
             _statusLabel.text = @"加载中，请稍后...";
-            CGSize size = [_statusLabel.text sizeWithFont:_statusLabel.font constrainedToSize:CGSizeMake(MAXFLOAT, 22)];
-            _indicatorView.frame = CGRectMake((self.frame.size.width - size.width) / 2 - 10 - 30, (self.frame.size.height - 25) / 2, 25, 25);
             [_indicatorView startAnimating];
             break;
         case PullUpRefreshLoadingAll:
